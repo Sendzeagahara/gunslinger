d32ac8101009ef53d8c1ed4c6bfcefb75b11bbcc
Sendzeagahara <padyshka0@gmail.com>
2024-07-19 15:18:26 +0300

    player movement animation

--- code/main.py
+++ code/main.py
@@ -23,16 +23,22 @@ class Game:
         self.setup()
 
         #sprites
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
 
     def setup(self):
         tilemap = load_pygame(join('..', 'data', 'maps', 'world.tmx'))
+
         for x, y, image in tilemap.get_layer_by_name('Ground').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
         for obj in tilemap.get_layer_by_name('Objects'):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
         for obj in tilemap.get_layer_by_name('Collisions'):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in tilemap.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
--- code/player.py
+++ code/player.py
@@ -4,14 +4,33 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_group):
         super().__init__(groups)
+        self.load_images()
+        self.state = 'down'
+        self.frame_index = 0
         self.image = pygame.image.load(join('..', 'images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
-        self.hitbox_rect = self.rect.inflate(-60, 0)
+        self.hitbox_rect = self.rect.inflate(-60, -90)
 
         #movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 500
         self.collision_sprites = collision_group
+
+    def load_images(self):
+        self.frames = {
+            'left': [],
+            'right': [],
+            'up': [],
+            'down': []
+        }
+
+        for state in self.frames.keys():
+            for folder_path, sub_folders, file_names in walk(join('..', 'images', 'player', state)):
+                if file_names:
+                    for file_name in sorted(file_names, key=lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -40,6 +59,18 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.x < 0:
                         self.hitbox_rect.left = sprite.rect.right
 
+    def animate(self, delta_time):
+        #get state
+        if self.direction.y != 0:
+            self.state = 'down' if self.direction.y > 0 else 'up'
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+
+        #animate
+        self.frame_index = self.frame_index + 5 * delta_time if self.direction else 0
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
     def update(self, delta_time):
         self.input()
         self.move(delta_time)
+        self.animate(delta_time)
