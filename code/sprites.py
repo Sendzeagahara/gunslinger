15750ae852c9495e65313ceb2ba01e28bb9019f4
Sendzeagahara <padyshka0@gmail.com>
2024-07-19 15:39:02 +0300

    gun sprite proper rotation

--- code/sprites.py
+++ code/sprites.py
@@ -1,6 +1,5 @@
-import pygame.sprite
-
 from settings import *
+from math import atan2, degrees
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -36,6 +35,15 @@ class Gun(pygame.sprite.Sprite):
         player_pos = pygame.math.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
         self.player_direction = (mouse_pos - player_pos).normalize()
 
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
     def update(self, _):
         self.get_direction()
+        self.rotate_gun()
         self.rect.center = self.player.rect.center + self.player_direction * self.distance
